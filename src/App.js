90fc1ba088353a93689963c69f7f2905041d73d8
LuisAlfredoDeLaOssaDiaz <luisdelaossadiaz27@gmail.com>
2022-05-06 22:51:40 -0500

    video 3 useRef, current.value, current.innetHTML

--- src/App.js
+++ src/App.js
@@ -1,48 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-const miConstante = <>
-  HOLA MUNDO
-</> ;
-
-const multiplicar = (a,b) => a+b;
-
-const btnPulsado = () => alert('boton pulsado');
-
+import {useRef} from 'react'
 
 function App() {
-  const valores = ['aaaaaaaaa','bbbbbbbbb','ccccccccc']
-  const elements = ['one', 'two', 'three'];
-  const items = []
 
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
 
-  for (const [index, value] of elements.entries()) {
-    console.log(index,value)
-    items.push(<li key={index}>{value}</li>)
+  const makeThings = _ => {
+    //alert(inputRef.current.value);
+    console.log(inputRef.current.value);
+    resultRef.current.innerHTML = inputRef.current.value
   }
   
   return (
-    <div className="App">
-      <header className="App-header">
-      <div className='nombre'>
-        <h1>{2+2}</h1>
-        <h1>{multiplicar(3,5)}</h1>
-      {miConstante}
-
-      <div>
-        <button onClick={btnPulsado}>BOTON</button>
+    <div>
+      <input type="text" ref={inputRef} />
+      <button onClick={makeThings}>hacer cosas</button>
+      <div ref={resultRef} >
       </div>
-      
-      {
-        valores.map(element => <p>{element}</p>)
-      }
-      <div>
-        {items}
-      </div>
-
-      </div>
-      </header>
     </div>
+    
   );
 }
 
