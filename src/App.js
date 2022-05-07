0c3a15ad9b0221b23a5ede46fb360f9448c994ca
LuisAlfredoDeLaOssaDiaz <luisdelaossadiaz27@gmail.com>
2022-05-07 00:17:59 -0500

    video 4 useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import {useRef} from 'react'
+import {useState, useEffect, useRef} from 'react'
 
-function App() {
 
-  const inputRef = useRef(null);
-  const resultRef = useRef(null);
+function App() {  
 
-  const makeThings = _ => {
-    //alert(inputRef.current.value);
-    console.log(inputRef.current.value);
-    resultRef.current.innerHTML = inputRef.current.value
-  }
+  const [a, setA] = useState(8);
+  const [text, setText] = useState('');
   
+  const hacerCosas = _ => {
+    const a = 4
+    setA(a)
+    console.log(a)
+  }
+
+  useEffect( _ => {
+      alert(a)
+  }, [a])
+  
+  const textChange = (event) => {
+    setText(event.target.value)
+    console.log('Luis Está Escribiendo...')
+  }
+
   return (
     <div>
-      <input type="text" ref={inputRef} />
-      <button onClick={makeThings}>hacer cosas</button>
-      <div ref={resultRef} >
-      </div>
+      
+      {`${a}`}
+
+      <button onClick={ _ => {hacerCosas()} }>Pulsame</button>
+
+      <input type="text" onChange={ event => textChange(event) } />
+      {text}
+
     </div>
     
   );
