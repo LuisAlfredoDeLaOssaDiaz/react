c8be8354c23374bf5275e3e1347f15e969c27f18
LuisAlfredoDeLaOssaDiaz <luisdelaossadiaz27@gmail.com>
2022-05-09 23:46:20 -0500

    Video 8 importar imagenes assets/img imagenes.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
+//import imagen1 from './assets/img/img3.jpg'
+import imagenes from './assets/imagenes';
 
 function App() {  
-  const condicion = false;
   return (
     <div>
-      { condicion && <h1>Hola</h1> } {/*SI CONDICION EXISTE O ES TRUE, IMPRIMIR HOLA*/}
-      { condicion ? <h1>Hola</h1> : <h1>Adios</h1> } {/*SI CONDICION EXISTE O ES TRUE IMPRIMIR HOLA, SI ES FALSO IMPRIMIR ADIOS*/}
+      { /* <img src={imagenes.imagen3} /> */ }
+      { /* <img src={imagenes[1]} /> */ }
+      <h1>{imagenes[2].title}</h1>
+      <img src={imagenes[2].imagen} />
     </div>
     
   );
